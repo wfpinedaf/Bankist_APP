99861d076bb43318f0437e6af4d6266eecfc20a2
wfpinedaf <wfpinedaf@outlook.com>
2023-07-25 23:40:04 -0500

    update

--- starter/script.js
+++ starter/script.js
@@ -1,7 +1,3 @@
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// BANKIST APP
-
 // Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
@@ -59,77 +55,5 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-/////////////////////////////////////////////////
-/*
-// Simple Array Methods
-let arr = ['a', 'b', 'c', 'd', 'e'];
-
-// SLICE
-console.log(arr.slice(2));
-console.log(arr.slice(2, 4));
-console.log(arr.slice(-2));
-console.log(arr.slice(-1));
-console.log(arr.slice(1, -2));
-console.log(arr.slice());
-console.log([...arr]);
-
-// SPLICE
-// console.log(arr.splice(2));
-arr.splice(-1);
-console.log(arr);
-arr.splice(1, 2);
-console.log(arr);
-
-// REVERSE
-arr = ['a', 'b', 'c', 'd', 'e'];
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
-console.log(arr2);
-
-// CONCAT
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
-
-// JOIN
-console.log(letters.join(' - '));
- 
-
-let arr = [23, 11, 64];
-console.log(arr.at(0));
-console.log(arr[0]);
-
-console.log(arr[arr.length - 2]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-console.log('---- FOREACH ----');
-movements.forEach(function (mov, i, arr) {
-  if (mov > 0) {
-    console.log(`Movement ${i + 1}: You deposited ${mov}`);
-  } else {
-    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
-  }
-});
-*/
-
-const consignacion = [200, 450, -400, 3000, -650, -130, 70, 1300];
-console.log(consignacion);
-console.log(consignacion.reverse);
-consignacion.forEach(function (con, i, arr) {
-  if (con > 0) {
-    console.log(`Consignacion ${i + 1}: Acabas de consignar ${con} `);
-  } else {
-    console.log(`Consignacion ${i + 1}: Acabas de retirar ${Math.abs(con)}`);
-  }
-});
+const displayMovements = function (movements) {};
+displayMovements(account1.movements);
