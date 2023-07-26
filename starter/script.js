c9a6e008ac4f30940481240712cc6109ff1fd9ef
wfpinedaf <wfpinedaf@outlook.com>
2023-07-25 23:52:41 -0500

    Fuction display array

--- starter/script.js
+++ starter/script.js
@@ -55,5 +55,18 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {};
+const displayMovements = function (movements) {
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}"> ${i + 1}</div>
+          <div class="movements__date">3 days ago</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
 displayMovements(account1.movements);
