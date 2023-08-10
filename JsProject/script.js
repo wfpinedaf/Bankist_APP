c94148a8e3d6e27c2dd7d4d0657bbfa4a83fbcfd
wfpinedaf <wfpinedaf@outlook.com>
2023-08-09 19:15:24 -0500

    update btn

--- JsProject/script.js
+++ JsProject/script.js
@@ -180,3 +180,37 @@ btnTransfer.addEventListener('click', function (e) {
     UpdateUI(currentAccount);
   }
 });
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    //ADD MOVEMENT
+    currentAccount.movements.push(amount);
+
+    //UPDATE UI
+    UpdateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    console.log(index);
+
+    //DELETE ACCOUNT
+    accounts.splice(index, 1);
+
+    //HIDE UD AND LOG OUT
+    containerApp.style.opacity = 0;
+  }
+  inputTransferAmount.value = inputTransferTo.value = '';
+});
